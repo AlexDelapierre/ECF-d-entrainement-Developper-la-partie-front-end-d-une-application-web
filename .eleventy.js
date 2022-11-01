8f9d573444dff87887032152bcc4cf21d3312d3b
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2022-11-01 18:30:03 +0100

    Add Bootstrap navbar in header

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
 
     // Pour cloner le dossier css dans le dossier public
+    eleventyConfig.addPassthroughCopy("./src/img");
     eleventyConfig.addPassthroughCopy("./src/css");
 
     // Pour générer le dossier public à partir du dossier src
